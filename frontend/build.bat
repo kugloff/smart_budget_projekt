41a081d6c1fb7dc8e95d209c94bfba7a86c32990
kisgergo9 <kisgergely1963@gmail.com>
2025-10-18 22:48:18 +0200

    Edit: build.bat ismétlés hozzáadva

--- frontend/build.bat
+++ frontend/build.bat
@@ -1,7 +1,9 @@
 @echo off
+chcp 65001
 setlocal
 set ERRHANDLER=0
-
+:start
+cls
 call npm install
 call npm run build
 
@@ -13,4 +15,6 @@ if errorlevel 1 (
     echo Success building!
 )
 
+echo Enter az újraépítéshez...
 pause
+goto start
